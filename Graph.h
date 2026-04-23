b86be1b73eeefa1c7e9777fd0b0db60d465b362b
Maymay-cpu <pengr0810@gmail.com>
2026-04-23 11:47:12 -0700

    I created Graph.h and Graph.cpp. And I wrote something in Graph.h.

---
+++ Graph.h
@@ -0,0 +1,25 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "Vertex.h"
+#include "Edge.h"
+#include <iostream>
+
+class Graph {
+    private:
+        int numVertices;
+        int numEdges;
+        int adjMatrix[1000][1000];
+        Vertex vertices[1000];
+        int oddNodes[1000];
+        int oddCount;
+
+    public:
+        Graph(int n, int m);
+        void addEdge(int u, int v, int w);     // add edge
+        void printAdjMatrix();                 // print matrix
+        void printOddDegreeNodes();            // print odd degree nodes
+        void dijkstra(int source);             // dijkstra
+};
+
+#endif
