af1c46a61b6bb0e7ce193fe850840475b4eca55f
Maymay-cpu <pengr0810@gmail.com>
2026-04-28 12:53:35 -0700

    part2 error fix

--- Main.cpp
+++ Main.cpp
@@ -16,13 +16,11 @@ int main(){
 
     Graph* g = new Graph(numOfVertices, numOfEdges);
 
-    while(!std::cin.eof()){
-        int startVertice;
-        int endVertice;
-        int weight;
-        std::cin >> startVertice;
-        std::cin >> endVertice;
-        std::cin >> weight;
+    int startVertice;
+    int endVertice;
+    int weight;
+    while(std::cin >> startVertice >> endVertice >> weight){
+        
         Edge* newEdge = new Edge(startVertice, endVertice);
         newEdge->setWeight(weight);
         // Here is where you load up the Graph object   
